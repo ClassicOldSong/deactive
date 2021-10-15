ed06b13ad45e359160cf3bb70b2c4f33d57366ae
Yukino Song <yukino@yuki.network>
2021-10-15 22:42:33 +0800

    Allow empty value for `ref`

--- main.d.ts
+++ main.d.ts
@@ -1,5 +1,5 @@
 type deactiveRefDestroyer = () => void
-type deactiveRefRegisterer = (key: string, val: any) => deactiveRefDestroyer
+type deactiveRefRegisterer = (key: string, val?: any) => deactiveRefDestroyer
 type deactiveRefStorage = { [key: string]: any }
 export type deactiveObserver = (key: string, val: any, oldVal?: any) => any
 export function deactive(observer: deactiveObserver): {
